0eba928a497f671c038d58fd8ca57b4b1c55d041
Gilliano Gonzalez <gilli@Gillianos-MBP.lan>
2018-06-21 01:10:41 -0500

    pushing files

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,17 @@
+var pgp = require('pg-promise');
+const cn = 'postgresql://gilli@localhost:5432/exaltedcb';
+var db = pgp(cn);
 var appRouter = function (app) {
     app.get("/", function(req, res) {
         res.status(200).send("Welcome to our restful API");
     });
 
     app.get("/user", function (req, res) {
-        res.json([{
-            id: 1,
-            username: "samsepi0l"
-        }, {
-            id: 2,
-            username: "D0loresH4ze"
-        }]);
+        db.any('select* from users').then(function(data) {
+            res.json(data);
+        }).catch(function(err) {
+            console.log('ERROR:', err);
+        });
     });
 }
   
