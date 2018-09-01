e4b48422f1efcae8a462ef01fb281c9f607b663f
Gilliano Gonzalez <gilli@Gillianos-MacBook-Pro.local>
2018-09-01 00:57:08 -0500

    adding sign-up endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -20,6 +20,43 @@ var appRouter = function (app) {
             }
         });
     });
+
+    app.get("/login", function(req, res) {
+        db.query('SELECT * FROM users WHERE username="' + req.username + '" AND password = crypt("' + req.password + '", password)', (err, response) => {
+            if (response) {
+                res.json(response.rows);
+            } else {
+                console.error(err);
+            }
+        });
+    });
+
+    app.get("/sign-up", function(req, res) {
+        const values = [req.email, req.first_name, req.last_name, req.username, req.password];
+        db.query(`insert into users 
+            (
+                email, 
+                first_name, 
+                last_name, 
+                username, 
+                password
+            ) 
+            values(
+                $1, 
+                $2, 
+                $3, 
+                $4, 
+                crypt($5, gen_salt('bf', 8))
+            )`, values, (err, response) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(200);
+                }
+            });
+    });
+
+
 }
   
 module.exports = appRouter;
