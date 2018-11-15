9ee6c259fac12166181451febc5ad86b5040c058
Gilliano Gonzalez <gilli@Gillianos-MBP.lan>
2018-11-15 01:05:41 -0600

    adding users endpoint

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -1,6 +1,15 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const bcrypt = require("bcrypt");
 const database_1 = require("../config/database");
 class UserRouter {
     constructor() {
@@ -18,7 +27,7 @@ class UserRouter {
         });
     }
     getUserById(req, res, next) {
-        database_1.db.query('select* from users where id = ' + req.params.id, (err, response) => {
+        database_1.db.query('select* from users where user_id = ' + req.params.id, (err, response) => {
             if (response) {
                 res.status(200).send({
                     message: 'Success',
@@ -34,9 +43,34 @@ class UserRouter {
             }
         });
     }
+    addUser(req, res, next) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const saltRounds = 10;
+                req.body.password = yield bcrypt.hash(req.body.password, saltRounds);
+                yield database_1.db.query(`insert into users (
+            username,
+            password,
+            email
+        ) values($1, $2, $3)`, [req.body.username, req.body.password, req.body.email]);
+                res.status(200).send({
+                    message: 'Success',
+                    status: res.status,
+                });
+            }
+            catch (err) {
+                res.status(404).send({
+                    message: err,
+                    status: res.status
+                });
+                throw new Error(err);
+            }
+        });
+    }
     init() {
         this.router.get('/', this.getAll);
         this.router.get('/:id', this.getUserById);
+        this.router.put('/add', this.addUser);
     }
 }
 exports.UserRouter = UserRouter;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import * as bcrypt from 'bcrypt';
 import { db } from '../config/database';
 
 export class UserRouter {
@@ -20,27 +21,48 @@ export class UserRouter {
   }
 
   public getUserById(req: Request, res: Response, next: NextFunction) {
-      db.query('select* from users where id = ' + req.params.id, (err, response) => {
-          if (response) {
-              res.status(200).send({
-                  message: 'Success',
-                  status: res.status,
-                  user: response.rows[0]
-              });
-          } else {
-              res.status(404).send({
-                message: err,
-                status: res.status
-              });
-          }
-      });
+    db.query('select* from users where user_id = ' + req.params.id, (err, response) => {
+        if (response) {
+            res.status(200).send({
+                message: 'Success',
+                status: res.status,
+                user: response.rows[0]
+            });
+        } else {
+            res.status(404).send({
+            message: err,
+            status: res.status
+            });
+        }
+    });
+  }
 
-
+  public async addUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const saltRounds: number = 10;
+        req.body.password = await bcrypt.hash(req.body.password, saltRounds);
+        await db.query(`insert into users (
+            username,
+            password,
+            email
+        ) values($1, $2, $3)`, [req.body.username, req.body.password, req.body.email]);
+        res.status(200).send({
+            message: 'Success',
+            status: res.status,
+        });
+    } catch(err) {
+        res.status(404).send({
+            message: err,
+            status: res.status
+        });
+        throw new Error(err);
+    }
   }
 
   init() {
     this.router.get('/', this.getAll);
     this.router.get('/:id', this.getUserById);
+    this.router.put('/add', this.addUser);
   }
 
 }
